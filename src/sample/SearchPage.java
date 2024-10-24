a6c556648b76f13afcf8db63df82609bf55cbf17
sonu-malewar <sona.malewar20@gmail.com>
2024-10-24 12:19:06 +0530

    locator and search() added

--- src/sample/SearchPage.java
+++ src/sample/SearchPage.java
@@ -1,9 +1,17 @@
 package sample;
 
 public class SearchPage {
+
 	
+	int a = 10;
+	int b = 20;
+
+	public String  searchItem() {
+		return "item got searched";
+	}
 	public static void main(String[] args) {
-		
+
+	
 		System.out.println("item is getting searched");
 	}
 
