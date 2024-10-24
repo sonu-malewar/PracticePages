2272b059508ed53db31590dffd19e885581238c9
sonu-malewar <sona.malewar20@gmail.com>
2024-10-24 12:50:31 +0530

    added substract(0 again

--- src/sample/LoginPage.java
+++ src/sample/LoginPage.java
@@ -3,10 +3,14 @@ package sample;
 public class LoginPage {
 	int a=10;
 	int b =30;
-	int c,d =20;
+	
 	// method name change from add() to sum()
 	public int sum(int a,int b) {
 		return a+b;
+	}
+	
+	public int substract(int a,int b) {
+		return a-b;
 	}
 	
 
