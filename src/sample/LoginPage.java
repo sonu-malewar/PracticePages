59ba97555452d5659ab5574946bb8b03085aa032
sonu-malewar <55089829+sonu-malewar@users.noreply.github.com>
2024-10-24 12:39:31 +0530

    Update LoginPage.java with var

--- src/sample/LoginPage.java
+++ src/sample/LoginPage.java
@@ -3,12 +3,13 @@ package sample;
 public class LoginPage {
 	int a=10;
 	int b =30;
+	int c,d =20;
 	// method name change from add() to sum()
 	public int sum(int a,int b) {
 		return a+b;
 	}
-	public int sub(int a,int b) {
-		return a-b;
+	public int sub(int c,int d) {
+		return c-d;
 	}
 
 }
